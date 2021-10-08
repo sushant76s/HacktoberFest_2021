98b00244e75855a91be7c5edbad519f9cd18477a
Hemant <87967889+Hemant2801@users.noreply.github.com>
2021-10-08 18:05:38 +0530

    Added a star pattern code.
    A simple code for printing star pattern and reverse star pattern.

---
+++ star_pattern.py
@@ -0,0 +1,31 @@
+﻿a=int(input("ENTER THE NUMBER OF ROWS:\n"))
+b=int(input("ENTER\n0:FOR REVERSE STAR PATTERN\n1:FOR STAR PATTERN5\n"))
+n=bool(b)
+print("\n")
+
+if n==True:
+	for i in range(0,a+1):
+			print("*"*int(i))
+	
+if n==False:
+	for i in range(a,0,-1):
+			print("*"*int(i))
+   
+   
+"""
+OUTPUT:
+
+ENTER THE NUMBER OF ROWS:
+5
+ENTER
+0:FOR REVERSE STAR PATTERN
+1:FOR STAR PATTERN        
+1
+
+*    
+**   
+***  
+**** 
+*****
+
+"""
